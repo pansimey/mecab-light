542e966c7bd06c045dae730f71ca2cea9a9f5670
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2013-02-13 20:45:12 +0900

    force_encoding for MeCab 0.994

--- lib/mecab-light.rb
+++ lib/mecab-light.rb
@@ -9,6 +9,6 @@ class Mecab
   end
 
   def parse(text)
-    Result.new(@tagger.parse(text).split(/\n/)[0..-2])
+    Result.new(@tagger.parse(text).force_encoding('UTF-8').split(/\n/)[0..-2])
   end
 end
