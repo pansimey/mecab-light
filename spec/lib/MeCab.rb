c440519bcaad0bcdc1cfacfd3c5bb049c3e2cf4f
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2013-02-27 00:13:15 +0900

    now testable without install MeCab

---
+++ spec/lib/MeCab.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+module MeCab
+  class Tagger
+    def parse(string)
+      "見る\t動詞,自立,*,*,一段,基本形,見る,ミル,ミル\nEOS\n"
+    end
+  end
+end
--- spec/mecab_spec.rb
+++ spec/mecab_spec.rb
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+$:.unshift(File.expand_path(File.dirname(__FILE__)) + '/lib')
+
 require 'rspec'
 require 'mecab-light'
 
