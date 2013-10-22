85c2b2a9f2c37005941e9b040554bfdd362deeb4
Hajime Wakahara <hajime.wakahara@e-harp.jp>
2013-10-22 17:00:12 +0900

    implement #2

--- ext/mecab/light/extconf.rb
+++ ext/mecab/light/extconf.rb
@@ -1,5 +1,12 @@
 require 'mkmf'
 
+begin
+  require 'Win32API'
+
+  dir_config('opt', 'c:\MeCab\sdk', 'c:\MeCab\bin')
+rescue LoadError
+end
+
 if have_header('mecab.h') && have_library('mecab')
   create_makefile('mecab/light/binding')
 else
