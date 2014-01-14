7181807e87826f67146b075c0e9135b8846334b4
Hajime Wakahara <hajime.wakahara@e-harp.jp>
2014-01-14 12:03:50 +0900

    add settings for code coverage

--- mecab-light.gemspec
+++ mecab-light.gemspec
@@ -21,4 +21,6 @@ Gem::Specification.new do |gem|
 
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'simplecov'
+  gem.add_development_dependency 'coveralls'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec'
+end
 
+require 'coveralls'
+Coveralls.wear!
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'mecab/light/morpheme'
 require 'mecab/light/result'
 require 'mecab/light/tagger'
+
 
 class MeCab::Light::Binding
 end
