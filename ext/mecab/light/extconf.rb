222c017aa5d7536e4a7e441485bf7e6a89ec07bf
Hajime Wakahara <hadzimme@icloud.com>
2014-03-30 18:17:25 +0900

    fix exconf.rb for Windows

--- ext/mecab/light/extconf.rb
+++ ext/mecab/light/extconf.rb
@@ -1,14 +1,13 @@
 require 'mkmf'
 
-begin
-  require 'Win32API'
-
-  dir_config('opt', 'c:\MeCab\sdk', 'c:\MeCab\bin')
-rescue LoadError
-end
-
-if have_header('mecab.h') && have_library('mecab')
-  create_makefile('mecab/light/binding')
+if mecab_dir = arg_config('--with-mecab-folder')
+  sdk_dir = File.join(mecab_dir, 'sdk')
+  bin_dir = File.join(mecab_dir, 'bin')
+  if find_header('mecab.h', sdk_dir) && find_library('mecab', nil, bin_dir)
+    create_makefile('mecab/light/binding')
+  end
 else
-  puts 'Install MeCab and give it another try.'
+  if have_header('mecab.h') && have_library('mecab')
+    create_makefile('mecab/light/binding')
+  end
 end
